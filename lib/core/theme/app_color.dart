b2935212209e0e8b8e0f3bf6d1ee796f44118e2e
Abdulmajid Usman <abstractmajid@gmail.com>
2022-12-25 21:45:11 +0100

    add colors

---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/animation.dart';
+
+abstract class AppColor {
+  static const white = Color(0XFFFFFFFF);
+  static const breakerBay = Color(0XFF5BA291);
+  static const mirage = Color(0XFF1C232D);
+  static const shark = Color(0XFF262830);
+  static const lima = Color(0XFF65BC27);
+  static const mySin = Color(0XFFFCAD20);
+}
